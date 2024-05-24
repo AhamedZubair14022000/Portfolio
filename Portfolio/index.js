5bb34cb2a7fc5ee2a972e9f23abaee378f956dec
Ahamed Zubair <127014141+AhamedZubair14022000@users.noreply.github.com>
2024-05-24 10:33:39 +0530

    index.js

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -1,30 +1,52 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navBar = document.querySelector('.navbar');
+const toggleButton = document.getElementById('menuIcon');
+const navContent = document.getElementById('navContent');
+const navbar = document.querySelector('.header');
+const homeEl = document.getElementById('home');
 
-menuIcon.addEventListener('click', function() {
-    menuIcon.classList.toggle('fa-xmark');
-    navBar.classList.toggle('active')
+toggleButton.addEventListener('click', function() {
+    navbar.classList.toggle('expanded');
+    if (navbar.classList.contains('expanded')) {
+        toggleButton.classList.add('fa-xmark');
+        // homeEl.classList.add('padding-added');
+    } else {
+        toggleButton.classList.remove('fa-xmark');
+        // homeEl.classList.remove('padding-added');
+    }
 });
 
 let sections = document.querySelectorAll('section');
-let navLinks =document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll('header nav a');
+let mailBtnEl = document.getElementById('mailBtn');
+mailBtnEl.addEventListener('click', function sendMail() {
 
-function sendMail() {
-    var params = {
-        from_name: document.getElementById("fullName").value,
-        email_id: document.getElementById("emailId").value,
-        mobile_no: document.getElementById("mobileNo").value,
-        subject: document.getElementById("emailSubject").value,
-        message: document.getElementById("emailContent").value,
+    const inputs = document.querySelectorAll('#contact-form input, #contact-form textarea');
+    let allFilled = true;
+
+    inputs.forEach(function(input) {
+        if (input.value.trim() === '') {
+            allFilled = false;
+        }
+    });
+
+    if (!allFilled) {
+        alert('Please fill in all the fields.');
+    } else {
+        var params = {
+            from_name: document.getElementById("fullName").value,
+            email_id: document.getElementById("emailId").value,
+            mobile_no: document.getElementById("mobileNo").value,
+            subject: document.getElementById("emailSubject").value,
+            message: document.getElementById("emailContent").value,
+        };
+        console.log(params);
+        emailjs.send("service_h4fy952", "template_02iw3f8", params)
+            .then(res => {
+                alert("Your Mail Send Successfully! " + res.status);
+                console.log(res.status);
+            })
+            .catch(error => alert("Error Occured Try Again! " + error.message));
     }
-    console.log(params);
-    emailjs.send("service_h4fy952", "template_02iw3f8", params)
-    .then(res => {
-        alert("Success! " + res.status);
-        console.log(res.status);
-    })
-    .catch(error => alert("Failure! " + error.message));
-}
+});
 
 
 window.onscroll = () => {
@@ -49,8 +71,9 @@ window.onscroll = () => {
     let header = document.querySelector('header');
     header.classList.toggle('sticky', top > 100);
 
-    menuIcon.classList.remove('fa-xmark');
-    navBar.classList.remove('active');
+    toggleButton.classList.remove('fa-xmark');
+    navbar.classList.remove('expanded');
+    homeEl.classList.remove('padding-added')
 };
 
 
@@ -60,10 +83,18 @@ ScrollReveal({
     delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, heading', {origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact-form', {origin: 'button'});
-ScrollReveal().reveal('.home-contact h1, .about-img', {origin: 'left'});
-ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right'});
+ScrollReveal().reveal('.home-content, heading', {
+    origin: 'top'
+});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, #contact-form', {
+    origin: 'button'
+});
+ScrollReveal().reveal('.home-contact h1, .about-img', {
+    origin: 'left'
+});
+ScrollReveal().reveal('.home-contact p, .about-content', {
+    origin: 'right'
+});
 
 const typed = new Typed('.text', {
     strings: ['Frontend Developer', 'Web Developer', 'Backend Developer', 'Full-Stack Developer'],
@@ -71,4 +102,5 @@ const typed = new Typed('.text', {
     backSpeed: 100,
     backDelay: 1000,
     loop: true,
+    showCursor: false,
 });
